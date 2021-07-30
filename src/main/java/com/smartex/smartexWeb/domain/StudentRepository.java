8701ba87157a88f129418d2b07cf7375f69f1eb7
ITAnna <zimanya@gmail.com>
2021-07-30 05:29:29 +0300

    sequence "user_sequence" created

---
+++ src/main/java/com/smartex/smartexWeb/domain/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.smartex.smartexWeb.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StudentRepository extends JpaRepository<User, Long> {
+
+
+
+}
--- src/main/java/com/smartex/smartexWeb/domain/User.java
+++ src/main/java/com/smartex/smartexWeb/domain/User.java
@@ -1,6 +1,22 @@
 package com.smartex.smartexWeb.domain;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table
 public class User {
+    @Id
+    @SequenceGenerator(
+            name = "user_sequence",
+            sequenceName = "user_sequence",
+            allocationSize = 1
+    )
+
+    @GeneratedValue(
+            strategy = GenerationType.SEQUENCE,
+            generator = "user_sequence"
+    )
 
     private Long id;
 
