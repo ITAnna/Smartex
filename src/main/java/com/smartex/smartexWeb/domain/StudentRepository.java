4aaac73843100c8146bea88441e872659c9707b9
ITAnna <zimanya@gmail.com>
2021-07-30 05:42:27 +0300

    database connection and findAll

--- src/main/java/com/smartex/smartexWeb/domain/StudentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smartex.smartexWeb.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<User, Long> {
-
-
-
-}
--- src/main/java/com/smartex/smartexWeb/service/UserService.java
+++ src/main/java/com/smartex/smartexWeb/service/UserService.java
@@ -1,7 +1,8 @@
 package com.smartex.smartexWeb.service;
 
 import com.smartex.smartexWeb.domain.User;
-import org.springframework.stereotype.Component;
+import com.smartex.smartexWeb.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,13 +10,15 @@ import java.util.List;
 @Service
 public class UserService {
 
-    public List<User> getUsers () {
-        return List.of(new User (
-                "Lesha",
-                "Ivanov",
-                "Ivanov@mail.ru",
-                "LeshaKalosha"
+    private final UserRepository userRepository;
 
-        ));
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+
+    public List<User> getUsers () {
+        return userRepository.findAll();
     }
 }
